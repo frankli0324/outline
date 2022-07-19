d72730c9f72e63eb848b47c80a6b21a1360d0241
Frank <frankli0324@hotmail.com>
2022-07-20 04:10:40 +0800

    fix merge error

--- server/utils/s3.ts
+++ server/utils/s3.ts
@@ -66,7 +66,7 @@ const s3config = {
   accessKeyId: AWS_ACCESS_KEY_ID,
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
   signatureVersion: "v4",
-});
+};
 
 s3config.endpoint = AWS_S3_ENDPOINT;
 const s3 = new AWS.S3(s3config);
