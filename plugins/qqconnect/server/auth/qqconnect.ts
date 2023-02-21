570447adc96ce5a0be68f0315a3d4943fdfeb05c
Frank <frankli0324@hotmail.com>
2023-02-22 03:33:24 +0800

    move qqconnect auth to plugin

---
+++ plugins/qqconnect/server/auth/qqconnect.ts
@@ -0,0 +1,132 @@
+import crypto from "crypto";
+import passport from "@outlinewiki/koa-passport";
+import type { Context } from "koa";
+import Router from "koa-router";
+import { Strategy } from "passport-oauth2";
+import accountProvisioner from "@server/commands/accountProvisioner";
+import env from "@server/env";
+import { AuthenticationError } from "@server/errors";
+import passportMiddleware from "@server/middlewares/passport";
+import { User } from "@server/models";
+import { AuthenticationResult } from "@server/types";
+import {
+  StateStore,
+  request,
+  getTeamFromContext,
+  getClientFromContext,
+} from "@server/utils/passport";
+
+const router = new Router();
+const providerName = "qqconnect";
+
+const QQ_CLIENT_ID = process.env.QQ_CLIENT_ID;
+const QQ_CLIENT_SECRET = process.env.QQ_CLIENT_SECRET;
+const QQ_SUBDOMAIN = process.env.QQ_SUBDOMAIN || "example.com";
+
+Strategy.prototype.userProfile = async function (accessToken, done) {
+  try {
+    const response = await request(env.OIDC_USERINFO_URI ?? "", accessToken);
+    return done(null, response);
+  } catch (err) {
+    return done(err);
+  }
+};
+
+if (QQ_CLIENT_ID && QQ_CLIENT_SECRET) {
+  passport.use(
+    providerName,
+    new Strategy(
+      {
+        authorizationURL: "https://graph.qq.com/oauth2.0/authorize",
+        tokenURL: "https://graph.qq.com/oauth2.0/token",
+        clientID: QQ_CLIENT_ID,
+        clientSecret: QQ_CLIENT_SECRET,
+        callbackURL: `${env.URL}/auth/${providerName}.callback`,
+        passReqToCallback: true,
+        scope: [],
+        // @ts-expect-error custom state store
+        store: new StateStore(),
+        state: true,
+        pkce: false,
+      },
+      // OpenID Connect standard profile claims can be found in the official
+      // specification.
+      // https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
+      // Non-standard claims may be configured by individual identity providers.
+      // Any claim supplied in response to the userinfo request will be
+      // available on the `profile` parameter
+      async function (
+        ctx: Context,
+        accessToken: string,
+        refreshToken: string,
+        params: { expires_in: number },
+        profile: Record<string, string>,
+        done: (
+          err: Error | null,
+          user: User | null,
+          result?: AuthenticationResult
+        ) => void
+      ) {
+        try {
+          if (profile.error) {
+            throw AuthenticationError(
+              "Unable to load user profile from QQ Connect API"
+            );
+          }
+          const team = await getTeamFromContext(ctx);
+          const client = getClientFromContext(ctx);
+          const openid = profile.openid;
+          const userInfo = await (
+            await fetch("https://graph.qq.com/user/get_user_info", {
+              method: "post",
+              headers: {
+                "content-type": "application/x-www-form-urlencoded",
+              },
+              body: `access_token=${accessToken}&openid=${openid}&oauth_consumer_key=${QQ_CLIENT_ID}`,
+            })
+          ).json();
+          console.log(userInfo);
+          const hash = crypto
+            .createHash("md5")
+            .update(userInfo.nickname)
+            .digest("hex");
+
+          const result = await accountProvisioner({
+            ip: ctx.ip,
+            team: {
+              teamId: team?.id,
+              name: "MBTI Mafia",
+              subdomain: QQ_SUBDOMAIN,
+            },
+            user: {
+              name: userInfo.nickname,
+              email: `${hash}@${QQ_SUBDOMAIN}`,
+              avatarUrl: userInfo.figureurl_qq_1,
+            },
+            authenticationProvider: {
+              name: providerName,
+              providerId: "mafia.mbti",
+            },
+            authentication: {
+              providerId: openid,
+              accessToken,
+              refreshToken,
+              scopes: [],
+            },
+          });
+          return done(null, result.user, { ...result, client });
+        } catch (err) {
+          return done(err, null);
+        }
+      }
+    )
+  );
+
+  router.get(providerName, passport.authenticate(providerName));
+
+  router.get(`${providerName}.callback`, passportMiddleware(providerName));
+}
+
+export const name = "QQ登陆";
+
+export default router;
